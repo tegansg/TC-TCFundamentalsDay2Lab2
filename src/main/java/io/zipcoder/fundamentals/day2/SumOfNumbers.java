2ea5f54f4c392d15e84ee9d6ecb3b5c15a6db4ff
Tegan Salter-George <tsaltergeorge@hotmail.com>
2017-08-10 11:30:20 +0100

    Finish lab

--- src/main/java/io/zipcoder/fundamentals/day2/SumOfNumbers.java
+++ src/main/java/io/zipcoder/fundamentals/day2/SumOfNumbers.java
@@ -1,9 +1,46 @@
 package io.zipcoder.fundamentals.day2;
 
+import java.util.Scanner;
 
 public class SumOfNumbers {
 
     public static void main(String[] args){
+    	
+    	SumOfNumbers sumOfNumbers = new SumOfNumbers();
+    	sumOfNumbers.askForNumber();
 
+    }
+    
+    public void askForNumber(){
+    	SumOfNumbers sumOfNumbers = new SumOfNumbers();
+    	
+    	Scanner in = new Scanner(System.in);
+    	System.out.print("Pick a number...");
+    	
+    	try 
+    	{
+        	int num = in.nextInt();
+        	int sumOfNum = sumOfNumbers.sumUpToNumber(num);
+    		String output = String.format("Sum of one to %d is %d", num, sumOfNum);
+        	System.out.print(output);
+   
+    	} catch(Exception e) {
+    		System.out.print("That's not a number silly.");
+    	}
+    		
+    	
+    	in.close();      
+    	
+    }
+    
+    private Integer sumUpToNumber(int n)
+    {
+    	int sumOfNum = 0;
+    	
+    	for(int i=1; i<=n; i++)
+    	{
+    		sumOfNum += i;
+    	}
+    	return sumOfNum;
     }
 }
